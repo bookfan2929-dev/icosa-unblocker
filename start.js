ea6263f6082c30729f6354fed1d0aac7da0cea4b
icosohedrondev <bookfan2929@gmail.com>
2026-06-11 20:12:36 -0500

    Remove popup_failed function and its checks
    Removed popup failure handling and related function.

--- start.js
+++ start.js
@@ -8,20 +8,10 @@ function olfunc() {
   </body>
   </html>
   `
-
-  if (!newWindow || newWindow.closed || typeof newWindow.closed === 'undefined') {
-    popup_failed();
-    return;
-  }
   
   newWindow.document.open();
   newWindow.document.write(customHtml);
   newWindow.document.close();
   
-  return;
-}
-
-function popup_failed() {
-  alert("If you're seeing this, you need to set popups to be allowed by default. Visit chrome://settings/content/popups and select \"Sites can send pop-ups and use redirects\", then refresh this page."
   return;
 }
