fa294a42d92aa1a0c5cba4ef3bb357abd4e14312
icosohedrondev <bookfan2929@gmail.com>
2026-06-11 19:03:16 -0500

    Inform users to enable pop-ups in browser settings
    Added a message to inform users about enabling pop-ups.

--- start.js
+++ start.js
@@ -12,5 +12,6 @@ function olfunc() {
   newWindow.document.open();
   newWindow.document.write(customHtml);
   newWindow.document.close();
+  document.body.innerHTML = "If you're seeing this, you need to set popups to be allowed by default. Visit <a href=\"chrome://settings/content/popups\">this</a> and select \"Sites can send pop-ups and use redirects\", then refresh this page."
   return;
 }
