6cb8b0ea53be78e1c390f4cee2a621f2ca559b86
icosohedrondev <bookfan2929@gmail.com>
2026-06-11 20:09:22 -0500

    Implement popup failure check and alert
    Add popup failure handling and alert message.

--- start.js
+++ start.js
@@ -9,9 +9,19 @@ function olfunc() {
   </html>
   `
 
+  if (!newWindow || newWindow.closed || typeof newWindow.closed === 'undefined') {
+    popup_failed();
+    return;
+  }
+  
   newWindow.document.open();
   newWindow.document.write(customHtml);
   newWindow.document.close();
-  document.body.innerHTML = "If you're seeing this, you need to set popups to be allowed by default. Visit <a href=\"chrome://settings/content/popups\">this</a> and select \"Sites can send pop-ups and use redirects\", then refresh this page."
+  
+  return;
+}
+
+function popup_failed() {
+  alert("If you're seeing this, you need to set popups to be allowed by default. Visit chrome://settings/content/popups and select \"Sites can send pop-ups and use redirects\", then refresh this page."
   return;
 }
